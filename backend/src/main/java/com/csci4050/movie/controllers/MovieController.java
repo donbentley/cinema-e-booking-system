7e6e3a492380220f9690abd6dbfa00a004d031c9
donbentley <donbentley@uga.edu>
2024-10-16 21:22:36 -0400

    added getall, getmovie, deletemovie, and updatemovie routing

--- backend/src/main/java/com/csci4050/movie/controllers/MovieController.java
+++ backend/src/main/java/com/csci4050/movie/controllers/MovieController.java
@@ -3,13 +3,13 @@ package com.csci4050.movie.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.csci4050.movie.request.*;
+import com.csci4050.movie.request.MovieRequest;
 import com.csci4050.movie.services.MovieService;
+import com.csci4050.movie.entities.Movie;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
@@ -25,6 +25,42 @@ public class MovieController {
             return new ResponseEntity<>(result, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieService.getAllMovies();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Movie> getMovie(@PathVariable Integer id) {
+        Movie movie = movieService.getMovieById(id);
+        if (movie != null) {
+            return new ResponseEntity<>(movie, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteMovie(@PathVariable Integer id) {
+        String result = movieService.deleteMovie(id);
+        if (result.equals("Movie deleted successfully")) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateMovie(@PathVariable Integer id, @RequestBody MovieRequest movieRequest) {
+        String result = movieService.updateMovie(id, movieRequest);
+        if (result.equals("Movie updated successfully")) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
         }
     }
 }
