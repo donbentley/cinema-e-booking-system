d5940159bea491ede5db44080911dc4e664d5b59
donbentley <donbentley@uga.edu>
2024-10-27 17:36:07 -0400

    edit profile added

--- booking/src/components/EditProfile.js
+++ booking/src/components/EditProfile.js
@@ -1,297 +1,132 @@
+import { Link } from 'react-router-dom';
+
 const EditProfile = () => {
 	return (
-		<form className="mx-auto mt-10 max-w-4xl p-6 md:p-10 bg-white rounded-lg shadow-lg" >
-			<div className="space-y-12">
-				<h2 className="text-base font-semibold leading-7 text-gray-900">
-					Edit Profile
-				</h2>
-
-				<div className="border-b border-gray-900/10 pb-8">
-					<p className="mt-1 text-sm leading-6 text-gray-600">
-						Update your info.
-					</p>
-
-					<div className="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
-						<div className="sm:col-span-1">
-							<label
-								htmlFor="first-name"
-								className="block text-sm font-medium leading-6 text-gray-900"
-							>
-								First name
-							</label>
-							<div className="mt-1">
-								<input
-									id="first-name"
-									name="first-name"
-									type="text"
-									autoComplete="given-name"
-									className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-								/>
-							</div>
-						</div>
-
-						<div className="sm:col-span-1">
-							<label
-								htmlFor="last-name"
-								className="block text-sm font-medium text-gray-900"
-							>
-								Last name
-							</label>
-							<div className="mt-1">
-								<input
-									id="last-name"
-									name="last-name"
-									type="text"
-									autoComplete="family-name"
-									className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-								/>
-							</div>
-						</div>
-
-						<div className="sm:col-span-2 lg:col-span-3">
-							<label
-								htmlFor="email"
-								className="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Email address
-							</label>
-							<div className="mt-1">
-								<input
-									id="email"
-									name="email"
-									type="email"
-									autoComplete="email"
-									className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-								/>
-							</div>
-						</div>
-
-						<div className="col-span-full">
-							<label
-								htmlFor="street-address"
-								className="block text-sm font-medium text-gray-900"
-							>
-								Street address
-							</label>
-							<div className="mt-1">
-								<input
-									id="street-address"
-									name="street-address"
-									type="text"
-									autoComplete="street-address"
-									className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-								/>
-							</div>
-						</div>
-
-						<div className="sm:col-span-1">
-							<label
-								htmlFor="city"
-								className="block text-sm font-medium text-gray-900"
-							>
-								City
-							</label>
-							<div className="mt-1">
-								<input
-									id="city"
-									name="city"
-									type="text"
-									autoComplete="address-level2"
-									className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-								/>
-							</div>
-						</div>
-
-						<div className="sm:col-span-1">
-							<label
-								htmlFor="region"
-								className="block text-sm font-medium text-gray-900"
-							>
-								State / Province
-							</label>
-							<div className="mt-1">
-								<input
-									id="region"
-									name="region"
-									type="text"
-									autoComplete="address-level1"
-									className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-								/>
-							</div>
-						</div>
-
-						<div className="sm:col-span-1">
-							<label
-								htmlFor="postal-code"
-								className="block text-sm font-medium text-gray-900"
-							>
-								ZIP / Postal code
-							</label>
-							<div className="mt-1">
-								<input
-									id="postal-code"
-									name="postal-code"
-									type="text"
-									autoComplete="postal-code"
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div className="border-b border-gray-900/10 pb-12">
+		<div className="mx-auto mt-10 max-w-4xl p-6 md:p-10 bg-white rounded-lg shadow-lg">
+			<form>
+				<div className="space-y-12">
 					<h2 className="text-base font-semibold leading-7 text-gray-900">
-						Notifications
+						Edit Profile
 					</h2>
-					<p className="mt-1 text-sm leading-6 text-gray-600">
-						We'll always let you know about important changes, but you pick what
-						else you want to hear about.
-					</p>
 
-					<div className="mt-10 space-y-10">
-						<fieldset>
-							<legend className="text-sm font-semibold leading-6 text-gray-900">
-								By Email
-							</legend>
-							<div className="mt-6 space-y-6">
-								<div className="relative flex gap-x-3">
-									<div className="flex h-6 items-center">
-										<input
-											id="comments"
-											name="comments"
-											type="checkbox"
-											className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-										/>
-									</div>
-									<div className="text-sm leading-6">
-										<label
-											htmlFor="comments"
-											className="font-medium text-gray-900"
-										>
-											Comments
-										</label>
-										<p className="text-gray-500">
-											Get notified when someones posts a comment on a posting.
-										</p>
-									</div>
-								</div>
-								<div className="relative flex gap-x-3">
-									<div className="flex h-6 items-center">
-										<input
-											id="candidates"
-											name="candidates"
-											type="checkbox"
-											className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-										/>
-									</div>
-									<div className="text-sm leading-6">
-										<label
-											htmlFor="candidates"
-											className="font-medium text-gray-900"
-										>
-											Candidates
-										</label>
-										<p className="text-gray-500">
-											Get notified when a candidate applies for a job.
-										</p>
-									</div>
-								</div>
-								<div className="relative flex gap-x-3">
-									<div className="flex h-6 items-center">
-										<input
-											id="offers"
-											name="offers"
-											type="checkbox"
-											className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-										/>
-									</div>
-									<div className="text-sm leading-6">
-										<label
-											htmlFor="offers"
-											className="font-medium text-gray-900"
-										>
-											Offers
-										</label>
-										<p className="text-gray-500">
-											Get notified when a candidate accepts or rejects an offer.
-										</p>
-									</div>
+					<div className="border-b border-gray-900/10 pb-8">
+
+						<div className="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
+							<div className="sm:col-span-1">
+								<label
+									htmlFor="first-name"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									First name
+								</label>
+								<div className="mt-1">
+									<input
+										id="first-name"
+										name="first-name"
+										type="text"
+										autoComplete="given-name"
+										className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+									/>
 								</div>
 							</div>
-						</fieldset>
-						<fieldset>
-							<legend className="text-sm font-semibold leading-6 text-gray-900">
-								Push Notifications
-							</legend>
-							<p className="mt-1 text-sm leading-6 text-gray-600">
-								These are delivered via SMS to your mobile phone.
-							</p>
-							<div className="mt-6 space-y-6">
-								<div className="flex items-center gap-x-3">
+
+							<div className="sm:col-span-1">
+								<label
+									htmlFor="last-name"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									Last name
+								</label>
+								<div className="mt-1">
 									<input
-										id="push-everything"
-										name="push-notifications"
-										type="radio"
-										className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+										id="last-name"
+										name="last-name"
+										type="text"
+										autoComplete="family-name"
+										className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 									/>
-									<label
-										htmlFor="push-everything"
-										className="block text-sm font-medium leading-6 text-gray-900"
-									>
-										Everything
-									</label>
-								</div>
-								<div className="flex items-center gap-x-3">
-									<input
-										id="push-email"
-										name="push-notifications"
-										type="radio"
-										className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-									/>
-									<label
-										htmlFor="push-email"
-										className="block text-sm font-medium leading-6 text-gray-900"
-									>
-										Same as email
-									</label>
-								</div>
-								<div className="flex items-center gap-x-3">
-									<input
-										id="push-nothing"
-										name="push-notifications"
-										type="radio"
-										className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-									/>
-									<label
-										htmlFor="push-nothing"
-										className="block text-sm font-medium leading-6 text-gray-900"
-									>
-										No push notifications
-									</label>
 								</div>
 							</div>
-						</fieldset>
+
+							<div className="sm:col-span-1">
+								<label
+									htmlFor="username"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									Username
+								</label>
+								<div className="mt-1">
+									<input
+										id="username"
+										name="username"
+										type="text"
+										autoComplete="username"
+										className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+									/>
+								</div>
+							</div>
+
+							<div className="sm:col-span-1">
+								<label
+									htmlFor="email"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									Email address
+								</label>
+								<div className="mt-1">
+									<input
+										id="email"
+										name="email"
+										type="email"
+										autoComplete="email"
+										className="block w-full rounded-md border py-2 px-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+									/>
+								</div>
+							</div>
+						</div>
+
+						<div className="mt-4 flex items-center">
+							<input
+								id="promotions"
+								name="promotions"
+								type="checkbox"
+								className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+							/>
+							<label htmlFor="promotions" className="ml-2 block text-sm text-gray-900">
+								Opt in for promotions
+							</label>
+						</div>
 					</div>
 				</div>
-			</div>
-			
 
-			<div className="mt-6 flex items-center justify-end gap-x-6">
-				<button
-					type="button"
-					className="text-sm font-semibold leading-6 text-gray-900"
+				<div className="mt-6 flex items-center justify-end gap-x-6">
+					<button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+						Cancel
+					</button>
+					<button
+						type="submit"
+						className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>
+						Save
+					</button>
+				</div>
+			</form>
+
+			<div className="mt-6 flex gap-4">
+				<Link
+					to="/reset-password"
+					className="rounded-md bg-gray-200 px-3 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-300"
 				>
-					Cancel
-				</button>
-				<button
-					type="submit"
-					className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					Reset Password
+				</Link>
+				<Link
+					to="/add-payment-card"
+					className="rounded-md bg-gray-200 px-3 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-300"
 				>
-					Save
-				</button>
+					Add Payment Card
+				</Link>
 			</div>
-		</form>
+		</div>
 	);
 };
 
