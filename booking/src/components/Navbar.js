fc9c7e1c4d4271a0fe6508b387f19489dd6f2b6b
donbentley <donbentley@uga.edu>
2024-10-29 12:01:56 -0400

    pre merge

--- booking/src/components/Navbar.js
+++ booking/src/components/Navbar.js
@@ -32,7 +32,9 @@ const Navbar = () => {
 		? [
 				{ name: "Profile", to: "/profile", current: false },
 				{ name: "Logout", to: "/", current: false, onClick: handleLogout },
-				...(userRole === "ROLE_ADMIN" ? [{ name: "Admin", to: "/admin", current: false }] : []),
+				...(userRole === "ROLE_ADMIN"
+					? [{ name: "Admin", to: "/admin", current: false }]
+					: []),
 		  ]
 		: [
 				{ name: "Log In", to: "/login", current: false },
@@ -50,8 +52,14 @@ const Navbar = () => {
 					<div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
 						<DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
 							<span className="sr-only">Open main menu</span>
-							<Bars3Icon className="block h-6 w-6 group-data-[open]:hidden" aria-hidden="true" />
-							<XMarkIcon className="hidden h-6 w-6 group-data-[open]:block" aria-hidden="true" />
+							<Bars3Icon
+								className="block h-6 w-6 group-data-[open]:hidden"
+								aria-hidden="true"
+							/>
+							<XMarkIcon
+								className="hidden h-6 w-6 group-data-[open]:block"
+								aria-hidden="true"
+							/>
 						</DisclosureButton>
 					</div>
 
@@ -68,7 +76,9 @@ const Navbar = () => {
 										to={item.to}
 										aria-current={item.current ? "page" : undefined}
 										className={classNames(
-											item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+											item.current
+												? "bg-gray-900 text-white"
+												: "text-gray-300 hover:bg-gray-700 hover:text-white",
 											"rounded-md px-3 py-2 text-sm font-medium"
 										)}
 										onClick={item.onClick ? item.onClick : null}
@@ -91,7 +101,9 @@ const Navbar = () => {
 							to={item.to}
 							aria-current={item.current ? "page" : undefined}
 							className={classNames(
-								item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+								item.current
+									? "bg-gray-900 text-white"
+									: "text-gray-300 hover:bg-gray-700 hover:text-white",
 								"block rounded-md px-3 py-2 text-base font-medium"
 							)}
 							onClick={item.onClick ? item.onClick : null}
